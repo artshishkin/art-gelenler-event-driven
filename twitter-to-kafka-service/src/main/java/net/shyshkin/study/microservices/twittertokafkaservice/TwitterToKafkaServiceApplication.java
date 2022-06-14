6c3e0e2bd37bcc11e884ad2c7343b27540385bb8
Art <d.art.shishkin@gmail.com>
2022-06-14 11:53:07 +0300

    7.2 ConfigurationProperties for service (#1)

--- twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -2,7 +2,9 @@ package net.shyshkin.study.microservices.twittertokafkaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication {
 
