a92a3cf35406f302807e17f60bc3298948d3f7ff
Art <d.art.shishkin@gmail.com>
2022-06-14 10:28:30 +0300

    7.1 The very first microservice - init Twitter-To-Kafka-Service (#1)

---
+++ twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.microservices.twittertokafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+}
