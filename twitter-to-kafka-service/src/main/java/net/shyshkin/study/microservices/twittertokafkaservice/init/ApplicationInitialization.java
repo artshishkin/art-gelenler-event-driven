4147163a520fe7ec5595e0e255ed72913738d6bd
Art <d.art.shishkin@gmail.com>
2022-06-14 12:48:17 +0300

    7.3 Logging Configuration (#1)

---
+++ twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/init/ApplicationInitialization.java
@@ -0,0 +1,20 @@
+package net.shyshkin.study.microservices.twittertokafkaservice.init;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.microservices.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ApplicationInitialization implements CommandLineRunner {
+
+    private final TwitterToKafkaServiceConfigData configData;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("Twitter-To-Kafka-Service Configuration Data: {}", configData);
+    }
+}
