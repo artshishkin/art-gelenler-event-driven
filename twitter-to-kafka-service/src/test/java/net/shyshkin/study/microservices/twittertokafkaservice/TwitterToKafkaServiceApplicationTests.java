3916756d24fff56d9d7043caa9e880625f8a716f
Art <d.art.shishkin@gmail.com>
2022-06-14 13:25:15 +0300

    Fixed Continuous Integration build (#1)

--- twitter-to-kafka-service/src/test/java/net/shyshkin/study/microservices/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
+++ twitter-to-kafka-service/src/test/java/net/shyshkin/study/microservices/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package net.shyshkin.study.microservices.twittertokafkaservice;
 
+import net.shyshkin.study.microservices.twittertokafkaservice.config.TwitterToKafkaServiceConfigData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TwitterToKafkaServiceApplicationTests {
 
+    @Autowired
+    private TwitterToKafkaServiceConfigData configData;
+
     @Test
     void contextLoads() {
+        assertThat(configData)
+                .hasNoNullFieldsOrProperties()
+                .hasFieldOrPropertyWithValue("welcomeMessage", "Welcome to twitter-to-kafka-service")
+                .satisfies(data -> assertThat(data.getTwitterKeywords())
+                        .hasSize(5)
+                        .allSatisfy(keyWord -> assertThat(keyWord).isNotEmpty()));
     }
 
 }
