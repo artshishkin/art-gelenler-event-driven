66965c62f6734e93aa0ebf7de2a88a3e96087bfb
Art <d.art.shishkin@gmail.com>
2022-06-15 13:17:57 +0300

    9.3 Filtering through Rules Endpoint (#1)

--- twitter-to-kafka-service-v2/src/main/java/net/shyshkin/study/microservices/twittertokafkaservicev2/config/TwitterConfig.java
+++ twitter-to-kafka-service-v2/src/main/java/net/shyshkin/study/microservices/twittertokafkaservicev2/config/TwitterConfig.java
@@ -3,15 +3,20 @@ package net.shyshkin.study.microservices.twittertokafkaservicev2.config;
 import com.twitter.clientlib.ApiException;
 import com.twitter.clientlib.TwitterCredentialsBearer;
 import com.twitter.clientlib.api.TwitterApi;
+import com.twitter.clientlib.model.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.microservices.twittertokafkaservicev2.executor.TweetsStreamListenersExecutor;
 import net.shyshkin.study.microservices.twittertokafkaservicev2.listener.TweetsStreamListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class TwitterConfig {
@@ -31,9 +36,17 @@ public class TwitterConfig {
 
         var tweetFields = Set.of("author_id", "id", "created_at");
         try {
-            InputStream streamResult = twitterApi().tweets().sampleStream(null, tweetFields, null, null, null, null, 0);
-            // sampleStream with TweetsStreamListenersExecutor
+//            deleteFilteringRules();
+            try {
+                GetRulesResponse rules = twitterApi().tweets().getRules(null, null, null);
+                log.debug("Rules: {}", rules);
+            } catch (Exception ex) {
+                addFilteringRules();
+            }
 
+            InputStream streamResult = twitterApi().tweets().searchStream(5, null, tweetFields, null, null, null, null, 0);
+
+            // sampleStream with TweetsStreamListenersExecutor
             TweetsStreamListenersExecutor tsle = new TweetsStreamListenersExecutor(streamResult);
             tsle.addListener(tweetsStreamListener);
             return tsle;
@@ -44,6 +57,31 @@ public class TwitterConfig {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void addFilteringRules() throws ApiException {
+
+        AddRulesRequest addRulesRequest = new AddRulesRequest();
+        List<RuleNoId> ruleNoIds = twitterToKafkaServiceConfigData.getTwitterKeywords()
+                .stream()
+                .map(keyword -> new RuleNoId().value(keyword))
+                .collect(Collectors.toList());
+        addRulesRequest.add(ruleNoIds);
+        AddOrDeleteRulesRequest addOrDeleteRulesRequest = new AddOrDeleteRulesRequest(addRulesRequest);
+        AddOrDeleteRulesResponse rulesResponse = twitterApi().tweets().addOrDeleteRules(10, addOrDeleteRulesRequest, false);
+        log.debug("Add Rules Response: {}", rulesResponse);
+    }
+
+    private void deleteFilteringRules() throws ApiException {
+
+        var deleteRulesRequestDelete = new DeleteRulesRequestDelete();
+        twitterToKafkaServiceConfigData
+                .getTwitterKeywords()
+                .forEach(deleteRulesRequestDelete::addValuesItem);
+
+        AddOrDeleteRulesRequest addOrDeleteRulesRequest = new AddOrDeleteRulesRequest(new DeleteRulesRequest().delete(deleteRulesRequestDelete));
+        AddOrDeleteRulesResponse rulesResponse = twitterApi().tweets().addOrDeleteRules(addOrDeleteRulesRequest, false);
+        log.debug("Delete Rules Response: {}", rulesResponse);
     }
 
 }
