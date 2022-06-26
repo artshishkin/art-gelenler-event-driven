d2684bf2ce4d7edb33b7064ec13918c4d49d9329
Art <d.art.shishkin@gmail.com>
2022-06-26 16:49:44 +0300

    19.3 Send message to Kafka Topic (TwitterKafkaStatusListener) (#1)

--- twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/listener/TwitterKafkaStatusListener.java
+++ twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/listener/TwitterKafkaStatusListener.java
@@ -1,17 +1,29 @@
 package net.shyshkin.study.microservices.twittertokafkaservice.listener;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.microservices.config.KafkaConfigData;
+import net.shyshkin.study.microservices.kafka.avro.model.TwitterAvroModel;
+import net.shyshkin.study.microservices.kafka.producer.service.KafkaProducer;
+import net.shyshkin.study.microservices.twittertokafkaservice.transformer.TwitterStatusToAvroTransformer;
 import org.springframework.stereotype.Component;
 import twitter4j.Status;
 import twitter4j.StatusAdapter;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class TwitterKafkaStatusListener extends StatusAdapter {
+
+    private final KafkaConfigData kafkaConfigData;
+    private final KafkaProducer<Long, TwitterAvroModel> producer;
+    private final TwitterStatusToAvroTransformer transformer;
 
     @Override
     public void onStatus(Status status) {
         log.debug("Status is: {}", status);
-        log.debug("Twitter status with text: {}", status.getText());
+        log.debug("Received status text {}, sending to kafka topic {}", status.getText(), kafkaConfigData.getTopicName());
+        TwitterAvroModel twitterAvroModel = transformer.getTwitterAvroModelFromStatus(status);
+        producer.sent(kafkaConfigData.getTopicName(), twitterAvroModel.getUserId(), twitterAvroModel);
     }
 }
