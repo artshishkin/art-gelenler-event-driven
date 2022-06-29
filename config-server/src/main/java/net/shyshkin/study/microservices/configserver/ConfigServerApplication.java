741a78db693ae71e94949ce08a4db84b64895d8c
Art <d.art.shishkin@gmail.com>
2022-06-29 15:12:12 +0300

    22.1 Creating Spring Cloud Config Server as a Microservice [skip ci] (#5)

---
+++ config-server/src/main/java/net/shyshkin/study/microservices/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package net.shyshkin.study.microservices.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigurationProperties
+@SpringBootApplication(scanBasePackages = {"net.shyshkin.study.microservices"})
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
