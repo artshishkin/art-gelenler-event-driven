a1db5ef3b70171ce3c279e20b6a640dcf71329ac
Art <d.art.shishkin@gmail.com>
2022-07-08 21:47:09 +0300

    #3 Refactor KafkaProducerConfigData to use Proper Types instead of Strings

--- app-config-data/src/main/java/net/shyshkin/study/microservices/config/KafkaProducerConfigData.java
+++ app-config-data/src/main/java/net/shyshkin/study/microservices/config/KafkaProducerConfigData.java
@@ -1,6 +1,8 @@
 package net.shyshkin.study.microservices.config;
 
 import lombok.Data;
+import org.apache.kafka.common.record.CompressionType;
+import org.apache.kafka.common.serialization.Serializer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "kafka-producer-config")
 public class KafkaProducerConfigData {
 
-    private String keySerializerClass;
-    private String valueSerializerClass;
-    private String compressionType;
+    private Class<? extends Serializer<?>> keySerializerClass;
+    private Class<? extends Serializer<?>> valueSerializerClass;
+    private CompressionType compressionType;
     private String acks;
     private Integer batchSize;
     private Integer batchSizeBoostFactor;
--- kafka/kafka-producer/src/main/java/net/shyshkin/study/microservices/kafka/producer/config/KafkaProducerConfig.java
+++ kafka/kafka-producer/src/main/java/net/shyshkin/study/microservices/kafka/producer/config/KafkaProducerConfig.java
@@ -31,7 +31,7 @@ public class KafkaProducerConfig<K extends Serializable, V extends SpecificRecor
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getValueSerializerClass());
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaProducerConfigData.getBatchSize() * kafkaProducerConfigData.getBatchSizeBoostFactor());
         props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaProducerConfigData.getLingerMs());
-        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType());
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType().name);
         props.put(ProducerConfig.ACKS_CONFIG, kafkaProducerConfigData.getAcks());
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerConfigData.getRequestTimeoutMs());
         props.put(ProducerConfig.RETRIES_CONFIG, kafkaProducerConfigData.getRetryCount());
