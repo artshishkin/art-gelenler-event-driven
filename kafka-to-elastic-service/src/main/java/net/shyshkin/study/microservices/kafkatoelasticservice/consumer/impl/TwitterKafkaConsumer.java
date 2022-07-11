a334dcb96ea6b95c79bf57b980c50fb246e9be8a
Art <d.art.shishkin@gmail.com>
2022-07-11 11:05:55 +0300

    34. Adding initialization check (#9)

--- kafka-to-elastic-service/src/main/java/net/shyshkin/study/microservices/kafkatoelasticservice/consumer/impl/TwitterKafkaConsumer.java
+++ kafka-to-elastic-service/src/main/java/net/shyshkin/study/microservices/kafkatoelasticservice/consumer/impl/TwitterKafkaConsumer.java
@@ -6,6 +6,9 @@ import net.shyshkin.study.microservices.config.KafkaConfigData;
 import net.shyshkin.study.microservices.kafka.avro.model.TwitterAvroModel;
 import net.shyshkin.study.microservices.kafka.client.KafkaAdminClient;
 import net.shyshkin.study.microservices.kafkatoelasticservice.consumer.KafkaConsumer;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -13,7 +16,9 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -23,6 +28,17 @@ public class TwitterKafkaConsumer implements KafkaConsumer<Long, TwitterAvroMode
     private final KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
     private final KafkaAdminClient kafkaAdminClient;
     private final KafkaConfigData kafkaConfigData;
+
+    @EventListener
+    public void onAppStarted(ApplicationStartedEvent event) {
+        Collection<TopicListing> topics = kafkaAdminClient.getTopics();
+        kafkaAdminClient.checkTopicsCreated(topics);
+        List<String> topicNames = topics.stream()
+                .map(TopicListing::name)
+                .collect(Collectors.toList());
+        log.debug("Topics with name `{}` are ready for operations", topicNames);
+        kafkaListenerEndpointRegistry.getListenerContainer("twitterTopicListener").start();
+    }
 
     @Override
     @KafkaListener(id = "twitterTopicListener", topics = "${kafka-config.topic-name}")
