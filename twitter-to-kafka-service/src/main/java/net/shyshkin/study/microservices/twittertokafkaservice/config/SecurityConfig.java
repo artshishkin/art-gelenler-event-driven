49e5d13bc5748451f79e4c6ad3875968ace7b2b3
Art <d.art.shishkin@gmail.com>
2022-07-15 10:06:16 +0300

    12.3 Docker Image of `twitter-to-kafka-service` with healthcheck (#12)

---
+++ twitter-to-kafka-service/src/main/java/net/shyshkin/study/microservices/twittertokafkaservice/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package net.shyshkin.study.microservices.twittertokafkaservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/actuator/health");
+    }
+
+}
