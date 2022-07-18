4e74526290ea2c114797d501aab5de0a0f67909c
Art <d.art.shishkin@gmail.com>
2022-07-18 08:12:25 +0300

    47.3 Security - NoAuth for healthcheck, Basic auth for other requests (#16)

--- elastic-query-service/src/main/java/net/shyshkin/study/microservices/elasticqueryservice/config/SecurityConfig.java
+++ elastic-query-service/src/main/java/net/shyshkin/study/microservices/elasticqueryservice/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package net.shyshkin.study.microservices.elasticqueryservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +15,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .antMatchers("/**").permitAll()
-                );
+                        .antMatchers("/actuator/health").permitAll()
+                        .antMatchers("/**").hasRole("USER")
+                )
+                .httpBasic()
+                .and()
+                .csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withUsername("art")
+                .password("{noop}artPassword")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
