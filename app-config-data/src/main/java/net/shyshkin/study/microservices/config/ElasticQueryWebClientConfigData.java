2abd3acb5cfc42227b8222f55a535d579396a0f0
Art <d.art.shishkin@gmail.com>
2022-07-20 15:53:54 +0300

    57.2 elastic-query-web-client Configuration Data (#17)

---
+++ app-config-data/src/main/java/net/shyshkin/study/microservices/config/ElasticQueryWebClientConfigData.java
@@ -0,0 +1,27 @@
+package net.shyshkin.study.microservices.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "elastic-query-web-client")
+public class ElasticQueryWebClientConfigData {
+
+    private Webclient webclient;
+
+    @Data
+    public static class Webclient {
+        private Long connectTimeoutMs;
+        private Long readTimeoutMs;
+        private Long writeTimeoutMs;
+        private Long maxInMemorySize;
+        private String contentType;
+        private String acceptType;
+        private URI baseUrl;
+    }
+
+}
--- elastic-query-web-client/src/test/java/net/shyshkin/study/microservices/elasticquerywebclient/ElasticQueryWebClientApplicationTests.java
+++ elastic-query-web-client/src/test/java/net/shyshkin/study/microservices/elasticquerywebclient/ElasticQueryWebClientApplicationTests.java
@@ -1,16 +1,41 @@
 package net.shyshkin.study.microservices.elasticquerywebclient;
 
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.microservices.config.ElasticQueryWebClientConfigData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
 @SpringBootTest
 @TestPropertySource(properties = {
         "spring.cloud.config.enabled=false"
 })
 class ElasticQueryWebClientApplicationTests {
 
+    @Autowired
+    ElasticQueryWebClientConfigData elasticQueryWebClientConfigData;
+
     @Test
     void contextLoads() {
+        log.debug("Elastic Query Web Client Config Data: {}", elasticQueryWebClientConfigData);
+        assertThat(elasticQueryWebClientConfigData)
+                .isNotNull()
+                .satisfies(data -> assertThat(data.getWebclient())
+                        .isNotNull()
+                        .satisfies(webclientData -> assertAll(
+                                () -> assertThat(webclientData.getConnectTimeoutMs()).isPositive(),
+                                () -> assertThat(webclientData.getReadTimeoutMs()).isPositive(),
+                                () -> assertThat(webclientData.getWriteTimeoutMs()).isPositive(),
+                                () -> assertThat(webclientData.getMaxInMemorySize()).isPositive(),
+                                () -> assertThat(webclientData.getContentType()).contains("application", "json"),
+                                () -> assertThat(webclientData.getAcceptType()).contains("application", "json"),
+                                () -> assertThat(webclientData.getBaseUrl())
+                                        .hasPath("/elastic-query-service/documents")
+                        )));
     }
 }
