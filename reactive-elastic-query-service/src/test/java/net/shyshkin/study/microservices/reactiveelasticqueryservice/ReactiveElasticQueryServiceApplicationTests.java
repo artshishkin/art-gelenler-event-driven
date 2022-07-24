27d0681806a3b95459d20ac62329834b82464e4e
Art <d.art.shishkin@gmail.com>
2022-07-24 11:01:00 +0300

    65.3 Test get-all-documents endpoint (#19)

--- reactive-elastic-query-service/src/test/java/net/shyshkin/study/microservices/reactiveelasticqueryservice/ReactiveElasticQueryServiceApplicationTests.java
+++ reactive-elastic-query-service/src/test/java/net/shyshkin/study/microservices/reactiveelasticqueryservice/ReactiveElasticQueryServiceApplicationTests.java
@@ -1,18 +1,131 @@
 package net.shyshkin.study.microservices.reactiveelasticqueryservice;
 
-import org.junit.jupiter.api.Disabled;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.microservices.config.UserConfigData;
+import net.shyshkin.study.microservices.elastic.model.index.impl.TwitterIndexModel;
+import net.shyshkin.study.microservices.reactiveelasticqueryservice.model.ElasticQueryServiceResponseModel;
+import net.shyshkin.study.microservices.reactiveelasticqueryservice.repository.TwitterElasticReactiveQueryRepository;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
-        "spring.cloud.config.enabled=false"
+        "spring.cloud.config.enabled=false",
+        "elastic-config.connection-url=http://${ELASTIC_HOST_ADDRESS}"
 })
-@Disabled("Can not run without Elasticsearch cluster")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Testcontainers
+@ContextConfiguration(initializers = ReactiveElasticQueryServiceApplicationTests.Initializer.class)
 class ReactiveElasticQueryServiceApplicationTests {
 
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    UserConfigData userConfigData;
+
+    @Container
+    static ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer();
+
+    @Autowired
+    TwitterElasticReactiveQueryRepository repository;
+
     @Test
-    void contextLoads() {
+    @Order(10)
+    void testInitCompleted() {
+
+        //given
+        TwitterIndexModel model = TwitterIndexModel.builder()
+                .id("1")
+                .userId(123L)
+                .text("Some test text")
+                .createdAt(ZonedDateTime.now())
+                .build();
+        repository.save(model).block();
+
+        //when
+        Awaitility.await()
+                .atMost(Duration.ofSeconds(5))
+                .until(
+                        () -> repository.count().block(),
+                        count -> count > 0
+                );
+        //then
+        log.debug("Init completed. ElasticRepository has {} elements", repository.count().block());
+    }
+
+    @Test
+    @Order(20)
+    void getAllDocuments_ok() {
+
+        //when
+        var responseBodyFlux = webTestClient
+                .get().uri("/documents")
+                .headers(httpHeaders -> httpHeaders.setBasicAuth(userConfigData.getUsername(), userConfigData.getPassword()))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .returnResult(ElasticQueryServiceResponseModel.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBodyFlux)
+                .consumeNextWith(model -> assertThat(model).isNotNull()
+                        .hasNoNullFieldsOrProperties()
+                        .hasFieldOrPropertyWithValue("id", "1")
+                        .hasFieldOrPropertyWithValue("text", "Some test text")
+                )
+                .verifyComplete();
+    }
+
+    @Test
+    @Order(30)
+    void getAllDocuments_ok_throughWebTestClient() {
+
+        //when
+        webTestClient.get().uri("/documents")
+                .headers(httpHeaders -> httpHeaders.setBasicAuth(userConfigData.getUsername(), userConfigData.getPassword()))
+                .exchange()
+
+                //then
+                .expectStatus().isOk()
+                .expectBodyList(ElasticQueryServiceResponseModel.class)
+                .hasSize(1)
+                .value(list -> assertThat(list.get(0))
+                        .isNotNull()
+                        .hasNoNullFieldsOrProperties()
+                        .hasFieldOrPropertyWithValue("id", "1")
+                        .hasFieldOrPropertyWithValue("text", "Some test text")
+                );
+    }
+
+    protected static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            String hostAddress = elasticsearchContainer.getHttpHostAddress();
+            System.setProperty("ELASTIC_HOST_ADDRESS", hostAddress);
+            log.debug("ELASTIC_HOST_ADDRESS: {}", hostAddress);
+        }
     }
 }
