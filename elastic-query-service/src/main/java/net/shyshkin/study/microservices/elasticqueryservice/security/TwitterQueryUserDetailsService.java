6218b5022d9a0a943b1cc9208c082eb0ed726763
Art <d.art.shishkin@gmail.com>
2022-08-23 15:00:58 +0300

    71.6 TwitterQueryUserDetailsService change (#20)

--- elastic-query-service/src/main/java/net/shyshkin/study/microservices/elasticqueryservice/security/TwitterQueryUserDetailsService.java
+++ elastic-query-service/src/main/java/net/shyshkin/study/microservices/elasticqueryservice/security/TwitterQueryUserDetailsService.java
@@ -1,17 +1,24 @@
 package net.shyshkin.study.microservices.elasticqueryservice.security;
 
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.microservices.elasticqueryservice.business.QueryUserService;
+import net.shyshkin.study.microservices.elasticqueryservice.transformer.UserPermissionsToUserDetailsTransformer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TwitterQueryUserDetailsService implements UserDetailsService {
+
+    private final QueryUserService queryUserService;
+    private final UserPermissionsToUserDetailsTransformer transformer;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return TwitterQueryUser.builder()
-                .username(username)
-                .build();
+        return queryUserService.findAllPermissionsByUsername(username)
+                .map(transformer::getUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with name: " + username));
     }
 }
