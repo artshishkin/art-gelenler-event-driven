1123333667536069212d8a53fb19eab9d4b9050b
Art <d.art.shishkin@gmail.com>
2022-09-22 11:31:16 +0300

    81.8 ContextLoads test (#37)

---
+++ analytics-service/src/test/java/net/shyshkin/study/microservices/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,21 @@
+package net.shyshkin.study.microservices.analyticsservice;
+
+import net.shyshkin.study.microservices.kafka.client.KafkaAdminClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+    @MockBean
+    JwtDecoder jwtDecoder;
+
+    @MockBean
+    KafkaAdminClient kafkaAdminClient;
+
+    @Test
+    void contextLoads() {
+    }
+}
